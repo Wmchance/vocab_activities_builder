7354e823f6ddc7b2530ae1ee51b17e1320daf076
Will <wmchance@gmail.com>
2023-03-02 14:41:48 +0700

    import vocab csv

--- practice.py
+++ practice.py
@@ -7,4 +7,5 @@ openai.api_key = OPENAI_API_KEY
 
 response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
 response_text = response.choices[0]['text']
+
 print(response_text)
